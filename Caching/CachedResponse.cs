a7a46a240231143fb11a2025cb928c0d5b564830
izenda-miguel <miguel.cervantes@izenda.com>
2017-11-07 13:00:08 -0500

    Added caching for certain endpoints using Nancy

---
+++ Caching/CachedResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace IzendaCustomBootstrapper.Caching
+{
+    /// <summary>
+    /// Wraps a regular response in a cached response
+    /// The cached response invokes the original response
+    /// </summary>
+    public class CachedResponse : Response
+    {
+        private readonly Response response;
+
+        public CachedResponse(Response response)
+        {
+            this.response = response;
+
+            this.ContentType = response.ContentType;
+            this.Headers = response.Headers;
+            this.StatusCode = response.StatusCode;
+            this.Contents = this.GetContents();
+        }
+
+        public override Task PreExecute(NancyContext context)
+        {
+            return this.response.PreExecute(context);
+        }
+
+        private Action<Stream> GetContents()
+        {
+            return stream =>
+            {
+                using (var memory = new MemoryStream())
+                {
+                    this.response.Contents.Invoke(memory);
+                    var contents = Encoding.ASCII.GetString(memory.ToArray());
+
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.Write(contents);
+                        writer.Flush();
+                    }
+                }
+            };
+        }
+    }
+}
