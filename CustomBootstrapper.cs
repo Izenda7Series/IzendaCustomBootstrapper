d62323d6880c92b705604ef1f46f227b6cc6d887
Seth Hutcheson <seth.hutcheson@izenda.com>
2018-03-05 13:57:36 -0500

    Initial Commit for removing data sources

---
+++ CustomBootstrapper.cs
@@ -0,0 +1,76 @@
+﻿using Izenda.BI.API.Bootstrappers;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using Izenda.BI.API.Helper;
+using Izenda.BI.Framework.Models;
+
+namespace Mvc5StarterKit.IzendaBoundary
+{
+    public class CustomBootstrapper : IzendaBootstraper
+    {
+        const string ApiPrefix = "api";
+
+        private JsonSerializerSettings _serializer;
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.AfterRequest.AddItemToEndOfPipeline(ctx =>
+            {
+                ModifyDataFormatEndpoint(ctx);
+            });
+
+            _serializer = new JsonSerializerSettings
+            {
+                DateTimeZoneHandling = DateTimeZoneHandling.Unspecified,
+                ContractResolver = new IzendaSerializerContractResolver()
+            };
+
+            base.RequestStartup(container, pipelines, context);
+        }
+
+
+        private void ModifyDataFormatEndpoint(NancyContext ctx)
+        {
+            if (!ctx.Request.Url.Path.Contains($"/{ApiPrefix}/report/field/dataFormat/"))
+                return;
+
+
+            List<DataFormat> formatList = new List<DataFormat>();
+
+
+            using (var memory = new MemoryStream())
+            {
+                ctx.Response.Contents.Invoke(memory);
+                var json = Encoding.UTF8.GetString(memory.ToArray());
+                formatList = JsonConvert.DeserializeObject<List<DataFormat>>(json);
+            }
+
+            ctx.Response.Contents = stream =>
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    foreach (var format in formatList.ToList())
+                    {
+                        if (format.Name.Contains("$") || format.Format.Contains("$"))
+                        {
+                            formatList.Remove(format);
+                        }
+                    }
+
+                    var json = JsonConvert.SerializeObject(formatList, _serializer);
+
+                    writer.Write(json);
+                    writer.Flush();
+                }
+            };
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Izenda Custom Bootstrapper
+
+ :warning: **This project is designed for demonstration purposes, please ensure that security and customization meet the standards of your company.**
+ 
+ 
+## Overview
+A custom bootstrapper allows you to modify requests and responses to the Izenda API. 
+
+## A Few Uses Cases
+1. Filtering down a large list of tenants based on some custom criteria.
+2. Removing items from filter field data.
+
+Currently, this repo only shows how to remove items from filter field data.
+
+## Required References:
+
+1. Izenda.BI.API.dll  
+2. Izenda.BI.Framework.dll 
+3. Nancy.dll
+
+## Installation
+
+1. Build the project and copy the dll to the bin folder of your Izenda API. 
+  
+   
+2. Update the Web.config (API) file to use the custom bootstrapper.
+```
+  <!--Izenda-->
+  <nancyFx>
+    <bootstrapper assembly="IzendaCustomBootstrapper" type="IzendaCustomBootstrapper.CustomBootstrapper" />
+  </nancyFx>
+  
+```
+4. Restart the API instance
